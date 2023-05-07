df1ab989247d30e41affcfda1d89f5c239ae52eb
m16std <uwm16@yandex.ru>
2023-05-07 19:29:21 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using Microsoft.Win32;
+using System.Windows.Controls;
+
+namespace tered
+{
+   
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using Microsoft.Win32;
+using System.Windows.Controls;
+using System.Drawing.Text;
+using System.Collections;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Controls.Primitives;
+
+namespace tered
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            cmbFontFamily.ItemsSource = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
+            cmbFontSize.ItemsSource = new List<double>() { 6, 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
+        }
+
+        private void RTB_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            object temp = RTB.Selection.GetPropertyValue(Inline.FontWeightProperty);
+            btnBold.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(FontWeights.Bold));
+            temp = RTB.Selection.GetPropertyValue(Inline.FontStyleProperty);
+            btnItalic.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(FontStyles.Italic));
+            temp = RTB.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
+            btnUnderline.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(TextDecorations.Underline));
+
+            temp = RTB.Selection.GetPropertyValue(Inline.FontFamilyProperty);
+            cmbFontFamily.SelectedItem = temp;
+            temp = RTB.Selection.GetPropertyValue(Inline.FontSizeProperty);
+            cmbFontSize.Text = temp.ToString();
+        }
+
+        private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+            if (dlg.ShowDialog() == true)
+            {
+                FileStream fileStream = new FileStream(dlg.FileName, FileMode.Open);
+                TextRange range = new TextRange(RTB.Document.ContentStart, RTB.Document.ContentEnd);
+                range.Load(fileStream, DataFormats.Rtf);
+            }
+        }
+
+        private void Save_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+            if (dlg.ShowDialog() == true)
+            {
+                FileStream fileStream = new FileStream(dlg.FileName, FileMode.Create);
+                TextRange range = new TextRange(RTB.Document.ContentStart, RTB.Document.ContentEnd);
+                range.Save(fileStream, DataFormats.Rtf);
+            }
+        }
+
+        private void cmbFontFamily_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cmbFontFamily.SelectedItem != null)
+                RTB.Selection.ApplyPropertyValue(Inline.FontFamilyProperty, cmbFontFamily.SelectedItem);
+        }
+
+        private void cmbFontSize_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            RTB.Selection.ApplyPropertyValue(Inline.FontSizeProperty, cmbFontSize.Text);
+        }
+
+        private void PrintCommand(object sender, RoutedEventArgs e)
+        {
+            PrintDialog pd = new PrintDialog();
+            if ((pd.ShowDialog() == true))
+            {
+                //use either one of the below
+                pd.PrintVisual(RTB as Visual, "printing as visual");
+                pd.PrintDocument((((IDocumentPaginatorSource)RTB.Document).DocumentPaginator), "printing as paginator");
+            }
+        }
+
+        private void SuperScript(object sender, RoutedEventArgs e)
+        {
+            var currentAlignment = RTB.Selection.GetPropertyValue(Inline.BaselineAlignmentProperty);
+
+            BaselineAlignment newAlignment = ((BaselineAlignment)currentAlignment == BaselineAlignment.Superscript) ? BaselineAlignment.Baseline : BaselineAlignment.Superscript;
+            RTB.Selection.ApplyPropertyValue(Inline.BaselineAlignmentProperty, newAlignment);
+        }
+
+        private void SubScript(object sender, RoutedEventArgs e)
+        {
+            var currentAlignment = RTB.Selection.GetPropertyValue(Inline.BaselineAlignmentProperty);
+
+            BaselineAlignment newAlignment = ((BaselineAlignment)currentAlignment == BaselineAlignment.Subscript) ? BaselineAlignment.Baseline : BaselineAlignment.Subscript;
+            RTB.Selection.ApplyPropertyValue(Inline.BaselineAlignmentProperty, newAlignment);
+        }
+
+        private void RTB_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        void richTextBox_ContextMenuOpening(object sender, ContextMenuEventArgs e)
+        {
+            // Sender must be RichTextBox.
+            RichTextBox rtb = sender as RichTextBox;
+            if (rtb == null) return;
+
+            ContextMenu contextMenu = rtb.ContextMenu;
+            contextMenu.PlacementTarget = rtb;
+
+            // This uses HorizontalOffset and VerticalOffset properties to position the menu,
+            // relative to the upper left corner of the parent element (RichTextBox in this case).
+            contextMenu.Placement = PlacementMode.RelativePoint;
+
+            // Compute horizontal and vertical offsets to place the menu relative to selection end.
+            TextPointer position = rtb.Selection.End;
+
+            if (position == null) return;
+
+            Rect positionRect = position.GetCharacterRect(LogicalDirection.Forward);
+            contextMenu.HorizontalOffset = positionRect.X;
+            contextMenu.VerticalOffset = positionRect.Y;
+
+            // Finally, mark the event has handled.
+            contextMenu.IsOpen = true;
+            e.Handled = true;
+        }
+    } 
+}
